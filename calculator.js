dc0b2fa0418f34e76990569f68040617867f312d
Nang <nangmonkham2016@gmail.com>
2022-10-05 10:56:57 +0700

    Add operator functions

---
+++ calculator.js
@@ -0,0 +1,21 @@
+// Operators
+const add = function(x, y) {
+    return x + y;
+}
+
+const subtract = function(x, y) {
+    return x - y;
+}
+
+const multiply = function(x, y) {
+    return x * y;
+}
+
+const divide = function(x, y) {
+    return x / y;
+}
+
+// operate function
+const operate = function (a, b, operator) {
+    return operator(a, b);
+}
