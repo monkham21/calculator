7bc47ce13dafe1c3ad65f05287fa65fdada86ca5
Nang MonKham <nangmonkham2016@gmail.com>
2022-10-13 17:19:45 +0700

    Modify calculator operation

--- calculator.js
+++ calculator.js
@@ -20,21 +20,17 @@ const divide = function(num1, num2) {
 }
 
 // operate function
-//const operate = function (num1, num2, operator) {
-//    return operator(num1, num2);
-//}
-
-// operate function
 const operate = {
-    '+': function(num1, num2) {return num1 + num2},
-    '-': function(num1, num2) {return num1 - num2},
-    '×': function(num1, num2) {return num1 * num2},
+    '+': function(num1, num2) {return Math.round((num1 + num2) * 1000) / 1000},
+    '-': function(num1, num2) {return Math.round((num1 - num2) * 1000) / 1000},
+    '×': function(num1, num2) {return Math.round((num1 * num2) * 1000) / 1000},
     '÷': function(num1, num2) {
             if (num2 == 0) {
                 return "Cannot divide by 0"
             } else {
-                return num1 / num2;
-            }}
+                return Math.round((num1 / num2) * 1000) / 1000;
+            }
+        }
 };
 
 // display function
@@ -48,41 +44,50 @@ const display = function(value, state) {
 }
 
 const click = function() {
-    var expression, state;
-    var num1 = "none", num2 = "none", operator = "none";
+    let state;
+    let num1 = "none", num2 = "none", operator = "none";
     document.querySelectorAll(".clicks").forEach(button => {
         button.onclick = function() {
-            expression = document.getElementById('display').innerHTML; // storing display value
 
-
+            // Keep displaying when only numbers are clicked
             if (button.className.indexOf("number") != -1) {
                 display(button.value, state);
                 state = "add";
-            } else if (button.className.indexOf("operator") != -1) {
-                if (operator == "none")
+            }
+            // Store numbers and operate when an operator is clicked
+            else if (button.className.indexOf("operator") != -1)
+            {
+                if (operator == "none" || operator =="=")
                 {
                     operator = button.value;
                     num1 = Number(document.getElementById('display').innerHTML);
-                    console.log(num1);
                     state = "new";
-                } else
-//                if (num1 != "none" && num2 == "none")
-                {
+                } else if (operator != "=") {
                     num2 = Number(document.getElementById('display').innerHTML);
-                    console.log(operate[operator](num1, num2));
                     document.getElementById('display').innerHTML = ``;
                     display(operate[operator](num1, num2));
+                    num1 = operate[operator](num1, num2);
                     state = "new";
-                    operator = "none";
+                    operator = button.value;
                 }
-//                else {
-//                    num1, num2 = "none";
-//                    state = "new";
-//                }
+            }
+            // Clear when clear button is clicked
+            else if (button.className.indexOf("clear") != -1) {
+                document.getElementById('display').innerHTML = ``;
+                state = "new";
+                num1, num2, operator = "none";
+            }
+
+            // Decimal button
+            let decimal = document.getElementById(".");
+            if (document.getElementById('display').innerHTML.indexOf(".") != -1 )
+            {
+                decimal.disabled = true;
+            } else {
+                decimal.disabled = false;
             }
         }
     })
-    return expression;
 }
 
 addEventListener('DOMContentLoaded', (event) => {
